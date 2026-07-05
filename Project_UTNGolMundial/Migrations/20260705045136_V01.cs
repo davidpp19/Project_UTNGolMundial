4d2f78a92f5e2109218e81107db7769bde0631ac
davidpp19 <kevdavidpena.06@gmail.com>
2026-07-05 00:03:47 -0500

    Se migro la base de datos

---
+++ Project_UTNGolMundial/Migrations/20260705045136_V01.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MiApi.UTNGolMundial.Migrations
+{
+    /// <inheritdoc />
+    public partial class V01 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fases",
+                columns: table => new
+                {
+                    Codigo = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: false),
+                    FechaInicio = table.Column<DateOnly>(type: "date", nullable: false),
+                    FechaFin = table.Column<DateOnly>(type: "date", nullable: false),
+                    Orden = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fases", x => x.Codigo);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Grupos",
+                columns: table => new
+                {
+                    Codigo = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<char>(type: "character(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grupos", x => x.Codigo);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sedes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: false),
+                    Ciudad = table.Column<string>(type: "text", nullable: false),
+                    Pais = table.Column<string>(type: "text", nullable: false),
+                    CapacidadAproximada = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sedes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Selecciones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CodigoFifa = table.Column<string>(type: "text", nullable: false),
+                    Nombre = table.Column<string>(type: "text", nullable: false),
+                    Confederacion = table.Column<string>(type: "text", nullable: false),
+                    EsAnfitrion = table.Column<bool>(type: "boolean", nullable: false),
+                    clasificacion = table.Column<string>(type: "text", nullable: false),
+                    GrupoCodigo = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Selecciones", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Selecciones_Grupos_GrupoCodigo",
+                        column: x => x.GrupoCodigo,
+                        principalTable: "Grupos",
+                        principalColumn: "Codigo",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Partidos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    NumeroPartidoFifa = table.Column<int>(type: "integer", nullable: false),
+                    FechaPartido = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Estado = table.Column<bool>(type: "boolean", nullable: false),
+                    GolesLocal = table.Column<int>(type: "integer", nullable: false),
+                    GolesVisitante = table.Column<int>(type: "integer", nullable: false),
+                    FaseCodigo = table.Column<int>(type: "integer", nullable: false),
+                    GrupoCodigo = table.Column<int>(type: "integer", nullable: false),
+                    SedeId = table.Column<int>(type: "integer", nullable: false),
+                    LocalId = table.Column<int>(type: "integer", nullable: false),
+                    VisitanteId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Partidos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Partidos_Fases_FaseCodigo",
+                        column: x => x.FaseCodigo,
+                        principalTable: "Fases",
+                        principalColumn: "Codigo",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Partidos_Grupos_GrupoCodigo",
+                        column: x => x.GrupoCodigo,
+                        principalTable: "Grupos",
+                        principalColumn: "Codigo",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Partidos_Sedes_SedeId",
+                        column: x => x.SedeId,
+                        principalTable: "Sedes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Partidos_Selecciones_LocalId",
+                        column: x => x.LocalId,
+                        principalTable: "Selecciones",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Partidos_Selecciones_VisitanteId",
+                        column: x => x.VisitanteId,
+                        principalTable: "Selecciones",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partidos_FaseCodigo",
+                table: "Partidos",
+                column: "FaseCodigo");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partidos_GrupoCodigo",
+                table: "Partidos",
+                column: "GrupoCodigo");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partidos_LocalId",
+                table: "Partidos",
+                column: "LocalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partidos_SedeId",
+                table: "Partidos",
+                column: "SedeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partidos_VisitanteId",
+                table: "Partidos",
+                column: "VisitanteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Selecciones_GrupoCodigo",
+                table: "Selecciones",
+                column: "GrupoCodigo");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Partidos");
+
+            migrationBuilder.DropTable(
+                name: "Fases");
+
+            migrationBuilder.DropTable(
+                name: "Sedes");
+
+            migrationBuilder.DropTable(
+                name: "Selecciones");
+
+            migrationBuilder.DropTable(
+                name: "Grupos");
+        }
+    }
+}
--- Project_UTNGolMundial/Program.cs
+++ Project_UTNGolMundial/Program.cs
@@ -1,23 +1,42 @@
-var builder = WebApplication.CreateBuilder(args);
+using Project_UTNGolMundial.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace MiApi.UTNGolMundial
 {
-    app.MapOpenApi();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<MiApiUTNGolMundialContext>(options =>
+            options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQL") ??
+                throw new InvalidOperationException("Connection string 'PostgreSQL' not found.")));
+
+            builder.Services.AddControllers()
+                .AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                );
+
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            var app = builder.Build();
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                //app.MapOpenApi(); //Esta línea se utiliza para mapear la documentación OpenAPI generada por NSwag a una ruta específica en la aplicación. Sin embargo, en este caso, no es necesaria porque el middleware de Swagger ya se encarga de exponer la documentación en la ruta predeterminada (/swagger).
+
+                app.UseSwagger();
+                app.UseSwaggerUI(); //La interfaz de usuario de Swagger se habilita solo en desarrollo, por seguridad. En producción, es recomendable deshabilitarla para evitar exponer información sensible sobre la API.
+            }
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
