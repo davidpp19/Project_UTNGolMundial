be3bab1ad653d570e39054217436b913d9ea89dd
davidpp19 <kevdavidpena.06@gmail.com>
2026-07-20 21:31:54 -0500

    Actualizacion de Modelos

---
+++ UTNGolMundial.Modelos/Auditoria.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTNGolMundial.Modelos
+{
+    public class Auditoria
+    {
+        [Key]
+        public long Id { get; set; }
+        public string TablaAfectada { get; set; }
+        public int RegistroId { get; set; }
+        public string Accion { get; set; }
+        public string DatosAnteriores { get; set; }
+        public string DatosNuevos { get; set; }
+        public DateTime Fecha { get; set; }
+
+        [ForeignKey("UsuarioId")]
+        public int UsuarioId { get; set; } //Clave foranea para relacionar con Usuario
+        public Usuario? Usuario { get; set; } //Relacion muchos a uno con Usuario, muchas acciones de auditoria pueden ser realizadas por un usuario
+    }
+}
---
+++ UTNGolMundial.Modelos/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTNGolMundial.Modelos
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Nombre { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public Boolean Activo { get; set; }
+
+    }
+}
